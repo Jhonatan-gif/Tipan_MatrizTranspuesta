8cdcee89301a4df6f2a8b016bb262571c3eb5043
Jhonatan Tipan <jhonatan.tipan@udl.edu.ec>
2023-11-27 16:14:39 -0500

    Inicializar programa y pedida de dimensiones

---
+++ Matriz_Transpuesta.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define Max 100
+
+int main (){
+    
+    int fil , col ;
+
+    //Pedir datos fias y columnas
+    printf ("Ingrese el numero de filas : ");
+    scanf ("%i",&fil);
+
+    printf ("Ingrese el numero de columnas : ");
+    scanf ("%i",&col);
+
+
+
+
+    return 0 ;
+}
